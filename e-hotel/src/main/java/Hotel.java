d412acdc001d169dbf9128da4d7d9a72ea92b46b
mfawaz092 <mfawazph@gmail.com>
2024-03-30 20:36:55 -0400

    hotel and hotelchain

---
+++ e-hotel/src/main/java/Hotel.java
@@ -0,0 +1,94 @@
+public class Hotel {
+    private int hotelID;
+    private String category;
+    private String name;
+    private String address;
+    private String email;
+    private String phone;
+    private int numberOfRooms;
+    private int stars;
+    private int chainID;
+
+    public Hotel(int hotelID, String category, String name, String address, String email, String phone, int numberOfRooms, int stars, int chainID) {
+        this.hotelID = hotelID;
+        this.category = category;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+        this.numberOfRooms = numberOfRooms;
+        this.stars = stars;
+        this.chainID = chainID;
+    }
+
+    public int getHotelID() {
+        return hotelID;
+    }
+
+    public void setHotelID(int hotelID) {
+        this.hotelID = hotelID;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public int getChainID() {
+        return chainID;
+    }
+
+    public void setChainID(int chainID) {
+        this.chainID = chainID;
+    }
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ e-hotel/src/main/java/HotelChain.java
@@ -0,0 +1,63 @@
+public class HotelChain {
+    private int chainID;
+    private String name;
+    private String address;
+    private int numberOfHotels;
+    private String email;
+    private String phone;
+    public HotelChain(int chainID, String name, String address, int numberOfHotels, String email, String phone) {
+        this.chainID = chainID;
+        this.name = name;
+        this.address = address;
+        this.numberOfHotels = numberOfHotels;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getChainID() {
+        return chainID;
+    }
+
+    public void setChainID(int chainID) {
+        this.chainID = chainID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getNumberOfHotels() {
+        return numberOfHotels;
+    }
+
+    public void setNumberOfHotels(int numberOfHotels) {
+        this.numberOfHotels = numberOfHotels;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
